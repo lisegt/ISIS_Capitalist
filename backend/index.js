7c66122c269f37b8ebc81c40d90175b78fa7b554
Lise Gauthier <lise-gauthier@live.fr>
2023-02-01 15:44:56 +0100

    accès images

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const app = express();
+app.use(express.static('public'));
 const { ApolloServer, gql } = require('apollo-server-express');
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql` type Query {
