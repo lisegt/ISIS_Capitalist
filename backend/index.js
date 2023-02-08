0dfef4756a0827c6e52fe5810fac9f42946e6c66
Lise Gauthier <lise-gauthier@live.fr>
2023-02-08 17:26:49 +0100

    adaptation du monde au joueur

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,29 @@ const resolvers = require("./resolvers")
 //inclure le monde complet défini dans world.js
 let world = require("./world")
 
+const fs = require("fs");
+
+//fonction pour que l'utilisateur récupère son monde et pas celui d'origine
+// fonction qui tente de lire le monde associé au joueur
+async function readUserWorld(user) {
+    try {
+        // on cherche à accéder au fichier du user
+        const data = await fs.readFile("userworlds/"+ user + "-world.json");
+        return JSON.parse(data);
+    }
+    catch (error) {
+        //on retourne le monde par défaut, notamment lors de la première connexion
+        return world
+    }
+}
+
 const server = new ApolloServer({
     typeDefs, resolvers,
     context: async ({ req }) => ({
-        world: world
+        //on appelle le monde du joueur connecté
+        world: await readUserWorld(req.headers["x-user"]),
+        //récupération de l'en-tête x-user ds requête http = récupérer nom du joueur dans contexte
+        user: req.headers["x-user"]
     })
 });
 
