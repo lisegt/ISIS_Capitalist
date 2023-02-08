928bcc362b6fa57a1d320daa90668b2bc8568b47
marionymd <marionn.aymm@gmail.com>
2023-02-08 17:26:49 +0100

    creation de div pour classes main, header et product + CSS correspondant

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+          <div> logo monde </div>
+          <div> argent </div>
+          <div> multiplicateur </div>
+          <div> ID du joueur </div>
       </header>
+        <main className="main">
+            <div> liste des boutons de menu </div>
+            <div className="product">
+                <div> premier produit</div>
+                <div> second produit</div>
+                <div> troisième produit</div>
+                <div> quatrième produit</div>
+                <div> cinquième produit</div>
+                <div> sixième produit</div>
+            </div>
+        </main>
     </div>
   );
 }
