23a0ebc4c10bbfa7ff8c8f343b777fd4d3defdef
marionymd <marionn.aymm@gmail.com>
2023-02-08 17:59:14 +0100

    requete get_world

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,78 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {gql} from "@apollo/client";
+
+const GET_WORLD=gql`
+    query getWorld {
+    getWorld {
+        name
+        logo
+        money
+        activeangels
+        angelbonus
+        lastupdate
+        allunlocks {
+            name
+            logo
+            seuil
+            idcible
+            ratio
+            typeratio
+            unlocked
+        }
+        angelupgrades {
+            name
+            logo
+            seuil
+            idcible
+            ratio
+            typeratio
+            unlocked
+        }
+        managers {
+            name
+            logo
+            seuil
+            idcible
+            ratio
+            typeratio
+            unlocked
+        }
+        upgrades {
+            name
+            logo
+            seuil
+            idcible
+            ratio
+            typeratio
+            unlocked
+        }
+        totalangels
+        score
+        products {
+            id
+            name
+            logo
+            cout
+            croissance
+            revenu
+            vitesse
+            quantite
+            timeleft
+            managerUnlocked
+            paliers {
+                name
+                logo
+                seuil
+                idcible
+                ratio
+                typeratio
+                unlocked
+            }
+        }
+    }
+}`
 
 function App() {
   return (
